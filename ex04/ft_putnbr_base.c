e40597a6492af21522f3489bc79444013a4b238c
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-02 11:29:50 +0200

    fix(ex04): fix formatting for norminette

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,71 +6,71 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 21:46:46 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/02 11:21:30 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/02 11:27:47 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return i;
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
 
-int ft_is_valid_base(char *base)
+int	ft_is_valid_base(char *base)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    if (ft_strlen(base) < 2)
-        return 0;
-    i = 0;
-    while (base[i] != '\0')
-    {
-        if (base[i] == '+' || base[i] == '-')
-            return 0;
-        j = i + 1;
-        while (base[j] != '\0')
-        {
-            if (base[i] == base[j])
-                return 0;
-            j++;
-        }
-        i++;
-    }
-    return 1;
+	if (ft_strlen(base) < 2)
+		return (0);
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
 
-void ft_print_base(long nb, char *base, int base_len)
+void	ft_print_base(long nb, char *base, int base_len)
 {
-    char c;
+	char	c;
 
-    if (nb >= base_len)
-        ft_print_base(nb / base_len, base, base_len);
-    c = base[nb % base_len];
-    write(1, &c, 1);
+	if (nb >= base_len)
+		ft_print_base(nb / base_len, base, base_len);
+	c = base[nb % base_len];
+	write(1, &c, 1);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-    long nb;
-    int base_len;
+	long	nb;
+	int		base_len;
 
-    if (!ft_is_valid_base(base))
-        return;
-    nb = (long)nbr;
-    base_len = ft_strlen(base);
-    if (nb < 0)
-    {
-        write(1, "-", 1);
-        nb = -nb;
-    }
-    ft_print_base(nb, base, base_len);
+	if (!ft_is_valid_base(base))
+		return ;
+	nb = (long)nbr;
+	base_len = ft_strlen(base);
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	ft_print_base(nb, base, base_len);
 }
 
 // #include <stdlib.h>
